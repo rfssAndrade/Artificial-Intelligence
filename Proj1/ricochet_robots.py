dec0cbd1e88861aac604386f119508d503320bff
danielquintas8 <daniel.quintas.lopes@gmail.com>
2020-10-24 11:27:08 +0100

    RBFS + comments

--- Proj1/ricochet_robots.py
+++ Proj1/ricochet_robots.py
@@ -10,9 +10,9 @@ from search import Problem, Node, astar_search, breadth_first_tree_search, \
     depth_first_tree_search, greedy_search, recursive_best_first_search
 import sys
 import copy
-import time
 import numpy as np
 import math
+
 
 
 class RRState:
@@ -40,6 +40,7 @@ class RRState:
 
 class Board:
     """ Representacao interna de um tabuleiro de Ricochet Robots. """
+
     def __init__(self):
         self.size = 0
         self.robots = {}
@@ -47,30 +48,66 @@ class Board:
         self.walls = {}
         self.cost_board = []
     
+
     def parsed_to_board(self, size, robots, target, walls):
+        """ Recebe os atributos do board parsed e converte para a representacao interna """
         self.size = size
         self.set_walls(walls)
         self.set_target(target[0], target[1])
         self.set_cost_board()
         self.set_robots(robots)
     
+
     def set_all(self, size, robots, target, walls, cost_board):
+        """ Mete os atributos a apontar para os pointers passados como argumentos """
         self.size = size
         self.robots = robots
         self.target = target
         self.walls = walls
         self.cost_board = cost_board
 
+
     def set_robots(self, robots):
+        """ Adiciona os robots """
         for robot in robots:
             self.set_robot(robot[0], robot[1])
 
 
     def set_robot(self, colour, position):
+        """ Adiciona um robot """
         self.robots[colour] = position
         pass
 
+
     def set_cost_board(self):
+        """ Cria o tabuleiro de custos """
+        def expand(position):
+            """ Expande uma posicao que teve o seu custo alterado e altera o custo de todas as posicoes a que tem acesso """
+            cost = self.cost_board[position[0]-1, position[1]-1] + 1
+
+            directions = []
+            for e in self.possible_moves_robot(self.target[0], position):
+                directions += e[1]
+            
+            result = []
+            original_position = position
+            for direction in directions:
+                position = original_position
+                while ((self.target[0], direction) in self.possible_moves_robot(self.target[0], position)):
+                    if direction == 'l' and self.cost_board[position[0]-1, position[1]-2] == -1:
+                        position = (position[0], position[1] - 1)
+                    elif direction == 'r' and self.cost_board[position[0]-1, position[1]] == -1:
+                        position = (position[0], position[1] + 1)
+                    elif direction == 'd' and self.cost_board[position[0], position[1]-1] == -1:
+                        position = (position[0] + 1, position[1])
+                    elif direction == 'u'and self.cost_board[position[0]-2, position[1]-1] == -1:
+                        position = (position[0] - 1, position[1])
+                    else:
+                        break
+                    result += [position]
+                    self.cost_board[position[0]-1, position[1]-1] = cost
+            return result
+
         self.cost_board = np.full((self.size, self.size), -1, dtype=int)
         self.cost_board[self.target[1][0]-1,self.target[1][1]-1] = 0
 
@@ -78,40 +115,11 @@ class Board:
 
         while(to_expand):
             next_position = to_expand.pop(0)
-            to_expand += self.expand(next_position)
+            to_expand += expand(next_position)
 
-    def expand(self, position):
-        cost = self.cost_board[position[0]-1, position[1]-1] + 1
-
-        directions = []
-        for e in self.possible_moves_robot(self.target[0], position):
-            directions += e[1]
-        
-        result = []
-        original_position = position
-        for direction in directions:
-            position = original_position
-            while ((self.target[0], direction) in self.possible_moves_robot(self.target[0], position)):
-                l = [position[0]-1, position[1]-2]
-                r = [position[0]-1, position[1]]
-                d = [position[0], position[1]-1]
-                u = [position[0]-2, position[1]-1]
-                if direction == 'l' and self.cost_board[position[0]-1, position[1]-2] == -1:
-                    position = (position[0], position[1] - 1)
-                elif direction == 'r' and self.cost_board[position[0]-1, position[1]] == -1:
-                    position = (position[0], position[1] + 1)
-                elif direction == 'd' and self.cost_board[position[0], position[1]-1] == -1:
-                    position = (position[0] + 1, position[1])
-                elif direction == 'u'and self.cost_board[position[0]-2, position[1]-1] == -1:
-                    position = (position[0] - 1, position[1])
-                else:
-                    break
-                if self.cost_board[position[0]-1, position[1]-1] == -1:
-                    result += [position]
-                    self.cost_board[position[0]-1, position[1]-1] = cost
-        return result
 
     def set_walls(self, walls):
+        """ Adiciona as walls """
         self.set_outside_walls()
 
         for wall in walls:
@@ -125,36 +133,46 @@ class Board:
             elif wall[1] == 'd':
                 self.set_wall((wall[0][0] + 1, wall[0][1]), 'u')
         
+
     def set_outside_walls(self):
+        """ Adiciona as walls exteriores """
         for i in range(1, self.size + 1):
             self.set_wall((i, 1), 'l')
             self.set_wall((1, i), 'u')
             self.set_wall((i, self.size), 'r')
             self.set_wall((self.size, i), 'd')
 
+
     def set_wall(self, position, side):
+        """ Adiciona uma parede """
         if position in self.walls.keys():
             self.walls[position] += side
         else:
             self.walls[position] = [side]
 
+
     def set_target(self, colour, position):
+        """ Adiciona o target """
         self.target = [colour, position]
+
 
     def robot_position(self, robot: str):
         """ Devolve a posição atual do robô passado como argumento. """
         return self.robots[robot]
         pass
     
-    def possible_moves(self):
-        moves = []
 
+    def possible_moves(self):
+        """ Devolve os movimentos possiveis """
+        moves = []
         for robot in self.robots.keys():
             moves += self.possible_moves_robot(robot, self.robot_position(robot))
         
         return moves
     
+
     def possible_moves_robot(self, robot, position):
+        """ Devolve os movimentos possiveis de um dado robot """
         moves = []
 
         if (position not in self.walls.keys() or 'l' not in self.walls[position]) and (position[0], position[1] - 1) not in self.robots.values():
@@ -167,7 +185,10 @@ class Board:
             moves.append((robot, 'd'))
         return moves
 
+
     def move_robot(self, action):
+        """ Move um robot segundo uma dada direcao """
+
         robot = action[0]
         position = self.robots[robot]
         direction = action[1]
@@ -185,6 +206,7 @@ class Board:
                 break
 
         self.robots[action[0]] = position
+
 
 
 def parse_instance(filename: str) -> Board:
@@ -213,18 +235,21 @@ def parse_instance(filename: str) -> Board:
     board.parsed_to_board(board_size, board_robots, board_target, board_walls)
     return board
 
+
+
 class RicochetRobots(Problem):
     def __init__(self, board: Board):
         """ O construtor especifica o estado inicial. """
-        # TODO: self.initial = ...
         self.initial = RRState(board)
         pass
+
 
     def actions(self, state: RRState):
         """ Retorna uma lista de ações que podem ser executadas a
         partir do estado passado como argumento. """
         return state.board.possible_moves()
         pass
+
 
     def result(self, state: RRState, action):
         """ Retorna o estado resultante de executar a 'action' sobre
@@ -241,12 +266,14 @@ class RicochetRobots(Problem):
         return RRState(board)
         pass
 
+
     def goal_test(self, state: RRState):
         """ Retorna True se e só se o estado passado como argumento é
         um estado objetivo. Deve verificar se o alvo e o robô da
         mesma cor ocupam a mesma célula no tabuleiro. """
         return state.board.robots[state.board.target[0]] == state.board.target[1]
         pass
+
 
     def h(self, node: Node):
         """ Função heuristica utilizada para a procura A*. """
@@ -257,27 +284,17 @@ class RicochetRobots(Problem):
         dx = abs(target_position[0] - robot_position[0])**2
         dy = abs(target_position[1] - robot_position[1])**2
         
-        # print("Id = ",node.state.id," Cost = ", node.state.board.cost_board[robot_position[0]-1][robot_position[1]-1])
         return math.sqrt(dx + dy) + node.state.board.cost_board[robot_position[0]-1][robot_position[1]-1]
 
 
+
 if __name__ == "__main__":
-    # Ler o ficheiro de input de sys.argv[1],
-    # Usar uma técnica de procura para resolver a instância,
-    # Retirar a solução a partir do nó resultante,
-    # Imprimir para o standard output no formato indicado.
-
-    # start = time.time()
-
     board = parse_instance(sys.argv[1])
-    # board = parse_instance("/Users/danielquintas8/Documents/GitHub/IA/Proj1/instances/i4.txt")
     problem = RicochetRobots(board)
-    # node = recursive_best_first_search(problem)
-    node = astar_search(problem)
+    node = recursive_best_first_search(problem)
 
     print(len(node.solution()))
     for e in node.solution():
         print(e[0], e[1])
     
-    # print(time.time() - start)
     pass
